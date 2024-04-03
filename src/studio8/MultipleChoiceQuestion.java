e4f2b998221b5e56166a2f6838623c371a6568c2
sofiarosenzweig <sofiarosenzweig@sofias-mbp.dhcp.wustl.edu>
2024-04-03 14:09:49 -0500

    doneeeeeee

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -2,15 +2,26 @@ package studio8;
 
 public class MultipleChoiceQuestion extends Question {
 	
+	private String[] choices;
+	
 	public MultipleChoiceQuestion(String prompt, String answer, int points, String[] choices) {
+		
 		//FIXME
 		
 		//Call the super class constructor, then create and set
 		//instance variables for any values that aren't handled
 		//by the base class
+		super(prompt, answer, points);
+		this.choices = choices;
 	}
 	
 	public void displayPrompt() {
+		super.displayPrompt();
+		for (int i = 0; i < choices.length; i++) {
+			System.out.println((i+1) + ": " + choices[i]);
+		}
+		
+		//choices[0] = 
 		//FIXME
 		
 		/*
@@ -25,6 +36,7 @@ public class MultipleChoiceQuestion extends Question {
 		 */
 		//
 	}
+	
 	
 	public static void main(String[] args) {
 		String[] choices = {"seven", "nine", "eight", "six"};
